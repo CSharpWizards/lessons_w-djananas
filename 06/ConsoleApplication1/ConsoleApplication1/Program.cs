3ea6c113eb409579523915fd1a6210bc5afeb67e
Evgenii1408 <32613778+Evgenii1408@users.noreply.github.com>
2018-03-29 20:39:40 +0300

    06
    06

---
+++ 06/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дороу! Сегодня помогу тебе определить кто ты по жизни. Ребенок,взрослый или старик!Все что необходимо ввести свой возвраст! Ну что уселся,пиши возвраст!");
+            int age = int.Parse(Console.ReadLine());
+            if (age <= 18)
+            {
+                Console.WriteLine("Ты ребенок! Можешь кушать леденцы на палочке");
+            }
+            else if (age < 70  )
+            {
+                Console.WriteLine("Ты взрослый!");
+            }
+            else if (age > 70)
+            {
+                Console.WriteLine("Ты старик! Иди в собес,бери пенсию,и сиди у подъезда на лавочке");
+            }
+            Console.WriteLine("Ну как помог? Если да,то оставь оценку на joycasino.net");
+            Console.ReadLine();
+            Console.WriteLine("Че ты тут тыкаешь? Помог,иди гуляй!!!!!!!!!!!!!!!!!!!!!");
+            Console.ReadLine();
+           
+
+
+
+        }
+    }
+}
---
+++ 06/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ПРЫВЕТ МЕДВЕД (Ака 2007)Сегодня я такому неудачнегу как ты Я??? ТЫЫЫ!ОККККККККК,КАРОЧИ ТЫ ЗАШЕЛ ЧТОБЫ УЗНАТЬ КАК ОБЫГРАТЬ ДРУГА В ШАХМАТЫ , А ТЫ НЕУДАЧНЕГ ЯЯЯЯ??? ДААААА ТЫЫЫ!!! НЕ УМЕЕШЬ ИГРАТЬ В ШАХМАТЫ И ЗАШЕЛ КО МНЕ СПРОСИТЬ,СМОЖЕТ ЛИ ТВОЙ КОНЬ ПЕРЕЙТИ С ОДНОЙ КЛЕТКИ НА ДРУГУЮ ОДНИМ ХОДОМ! (ДААА ИМЕННО ДЛЯ ЭТОГО..) МОЛЧАТЬ! ПИШИ СТОЛБЫ И СТРОЧКИ,А Я ТАК УЖ И БЫТЬ ТЕБЕ ПОМОГУ,НЕЕЕУДАЧНЕГ (ЯЯЯ) ПОШЕЛ НАХ**");
+            int strok1 = int.Parse(Console.ReadLine());
+            int stolb1 = int.Parse(Console.ReadLine());
+            int strok2 = int.Parse(Console.ReadLine());
+            int stolb2 = int.Parse(Console.ReadLine());
+
+            if (strok1 - 2 == strok2 && stolb1 + 1 == stolb2)
+            {
+
+
+                Console.WriteLine("ОК");
+            }
+            else
+            {
+                Console.WriteLine("НОУ");
+            }
+
+            Console.ReadLine();
+
+            {
+            
+            }
+         
+            
+
+        }
+    }
+}
